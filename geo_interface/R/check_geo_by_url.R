a26fad82bab89abd8973ed1e58ce5d04b148af54
Nick Giangreco <npg2108@cumc.columbia.edu>
2020-10-22 23:58:06 -0400

    stevens et al data application

--- geo_interface/R/check_geo_by_url.R
+++ geo_interface/R/check_geo_by_url.R
@@ -4,10 +4,12 @@ check_geo_by_url  <- function(id_input = "GSE15896"){
   url   <- case_when(
     grepl("^GSE",id_input) ~ 
       paste0("https://ftp.ncbi.nlm.nih.gov/geo/series/",
-             substr(id_input,1,6),"nnn/GSE",
+             substr(id_input,1,nchar(id_input)-3),"nnn/GSE",
              substr(id_input,4,stop = nchar(id_input))),
     grepl("^GDS",id_input) ~
-      paste0("https://ftp.ncbi.nlm.nih.gov/geo/datasets/GDSnnn/GDS",
-             substr(id_input,start = 4,stop = nchar(id_input))))
+      paste0("https://ftp.ncbi.nlm.nih.gov/geo/datasets/",
+             substr(id_input,1,nchar(id_input)-3),"nnn/GDS",
+             substr(id_input,4,stop = nchar(id_input)))
+  )
   return(url.exists(url))
 }
